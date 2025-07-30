cdf7c227fa76547c2a851b9172d8406e484babe5
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-07-30 16:57:58 -0400

    4

--- Scripts/Towers/tower.gd
+++ Scripts/Towers/tower.gd
@@ -3,4 +3,6 @@ extends Node2D
 
 @export var valid_upgrades: Array[Upgrade]
 
+var loop_interior: bool = true
+
 var current_upgrades: Array[Upgrade]
---
+++ Scripts/conveyor_belt.gd
@@ -0,0 +1,5 @@
+class_name ConveyorBelt
+extends Line2D
+
+func update_points() -> void:
+	pass
