1c8e49ca5d1099dd6580cc4c439c6a3565ab828e
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-07-30 19:05:01 -0400

    8 - Convex Hull For Conveyor

---
+++ Scenes/conveyor_belt_generator.gd
@@ -0,0 +1,60 @@
+extends Line2D
+
+
+func _ready() -> void:
+	var pins_parent := $"../Pins"
+	var pins_positions: Array[Vector2] = []
+	
+	for pin in pins_parent.get_children():
+		pins_positions.append(pin.global_position)
+	
+	var polygon = _find_polygon(pins_positions)
+	points = polygon
+
+
+func _find_polygon(points) -> Array[Vector2]:
+	#var polygon: Array[Vector2] = [
+		#Vector2(-50, -50),
+		#Vector2(10, 75),
+		#Vector2(30, 45),
+		#Vector2(-50, -50)
+	#]
+	
+	var polygon: Array[Vector2] = []
+	var sorted_points := _sort_points(points)
+	
+	if len(sorted_points) <= 1:
+		return sorted_points.duplicate()
+	
+	var lower: Array[Vector2] = []
+	for p in sorted_points:
+		while len(lower) >= 2 and orient(lower[-2], lower[-1], p) <= 0:
+			lower.pop_at(-1)
+		lower.append(p)
+	
+	sorted_points.reverse()
+	
+	var upper: Array[Vector2] = []
+	for p in sorted_points:
+		while len(upper) >= 2 and orient(upper[-2], upper[-1], p) <= 0:
+			upper.pop_at(-1)
+		upper.append(p)
+	
+	polygon = lower.slice(0, lower.size() - 1) + upper.slice(0, upper.size() - 1)
+	
+	return polygon
+
+
+func _sort_points(points) -> Array[Vector2]:
+	var sorted_points: Array[Vector2] = []
+	
+	sorted_points = points.duplicate() as Array[Vector2]
+	sorted_points.sort_custom(func(a: Vector2, b: Vector2) -> bool:
+		return (a.x < b.x) or (is_equal_approx(a.x, b.x) and a.y < b.y)
+	)
+	
+	return sorted_points
+
+
+func orient(origin, point_a, point_b) -> float:
+	return (point_a.x - origin.x) * (point_b.y - origin.y) - (point_a.y - origin.y) * (point_b.x - origin.x)
