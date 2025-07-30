f618717a94759cf3b9879341ddae77d6cd7b5066
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-07-30 18:22:40 -0400

    6

--- Scripts/Towers/tower.gd
+++ Scripts/Towers/tower.gd
@@ -2,13 +2,22 @@ class_name Tower
 extends Node2D
 
 @export var valid_upgrades: Array[Upgrade]
+@export var attack_radius: float = 80.0
+@onready var radius_area: CollisionShape2D = $AttackArea/RadiusArea
+@onready var placements: Node2D = $Placements
 
 var loop_interior: bool = true
 
 var current_upgrades: Array[Upgrade]
+
+func _ready() -> void:
+	update_stats()
 
 func _on_area_2d_mouse_entered() -> void:
 	Game.get_input_handler().hovered_tower = self
 
 func _on_area_2d_mouse_exited() -> void:
 	Game.get_input_handler().hovered_tower = null
+
+func update_stats() -> void:
+	radius_area.shape.radius = attack_radius
--- Scripts/conveyor_belt.gd
+++ Scripts/conveyor_belt.gd
@@ -1,5 +1,10 @@
 class_name ConveyorBelt
 extends Line2D
 
+@export var all_points:PackedVector2Array
+
+func add_tower(tower: Tower) -> void:
+	pass
+
 func update_points() -> void:
 	pass
