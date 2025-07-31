917693f142a3eccf83990bbc494ea5427b339f45
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-07-31 19:59:07 -0400

    25 - Lifesaving Adjustments... Trust

--- Scripts/selection_handler.gd
+++ Scripts/selection_handler.gd
@@ -3,21 +3,28 @@ extends Node
 
 var selected_tower: Tower
 var hovered_tower: Tower
+var show_radius_tower: Tower
 
 var menu_select: Tower
 
 @onready var wave_manager: WaveManager = $"../WaveManager"
 
 func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_pressed("LeftClick"):
+	if Input.is_action_just_pressed("LeftClick"):
 		if hovered_tower:
 			selected_tower = hovered_tower
 			if menu_select:
 				menu_select.unselect()
 			menu_select = hovered_tower
 			selected_tower.select()
-	else:
+			show_radius_tower = selected_tower
+		elif show_radius_tower:
+			show_radius_tower.unselect()
+			show_radius_tower = null
+	
+	if Input.is_action_just_released("LeftClick"):
 		selected_tower = null
+	
 	if Input.is_action_just_pressed("PlayNextWave"):
 		wave_manager.play_next_wave()
 
