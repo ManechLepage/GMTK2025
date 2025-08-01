2550ba01e1837a04b1577d8b987bfa4c45d5019d
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-07-31 22:55:14 -0400

    35

--- Scripts/Towers/upgrade_slot.gd
+++ Scripts/Towers/upgrade_slot.gd
@@ -15,6 +15,14 @@ func _ready() -> void:
 
 func load_upgrade(_upgrade: Upgrade) -> void:
 	upgrade = _upgrade
+	name_label.text = upgrade.name
+	
+	for item: Item in upgrade.cost.keys():
+		var item_cost: Control = ITEM_COST.instantiate()
+		cost.add_child(item_cost)
+		
+		item_cost.get_child(0).text = str(upgrade.cost[item])
+		item_cost.get_child(1).texture = item.texture
 
 func _on_mouse_entered() -> void:
 	Game.get_tower_menu().show_description(upgrade)
