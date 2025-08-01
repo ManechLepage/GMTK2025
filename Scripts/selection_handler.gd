9ee590484a1d2787afdd40fe7beabfbeb203fbca
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-07-31 22:51:16 -0400

    34 - FIXED SELECTION yay

--- Scripts/selection_handler.gd
+++ Scripts/selection_handler.gd
@@ -6,6 +6,7 @@ var hovered_tower: Tower
 var show_radius_tower: Tower
 
 var tower_offset = null
+var can_drag_tower: bool = false
 
 var menu_select: Tower
 
@@ -25,7 +26,7 @@ func _process(delta: float) -> void:
 		did_move_mouse = true
 		last_mouse_pos = mouse_pos
 	
-	if selected_tower:
+	if selected_tower and can_drag_tower:
 		if tower_offset != null:
 			selected_tower.position = selected_tower.get_global_mouse_position() + tower_offset
 		else:
@@ -36,35 +37,35 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("LeftClick"):
 		did_move_mouse = false
 		if hovered_tower:
-			if menu_select != null and hovered_tower != menu_select:
-				menu_select.unselect()
+			if selected_tower != null and hovered_tower != selected_tower:
+				selected_tower.unselect()
+			
 			selected_tower = hovered_tower
-			#if menu_select == hovered_tower:
-				#menu_select.unselect()
-				#menu_select = null
-				#return
-			#if menu_select:
-				#menu_select.unselect()
-			menu_select = hovered_tower
 			selected_tower.select()
 			show_radius_tower = selected_tower
 			
 			tower_offset = hovered_tower.global_position - main.get_global_mouse_position()
 			
+			can_drag_tower = true
 			_select_tower(hovered_tower)
 
-		elif show_radius_tower:
-			show_radius_tower.unselect()
-			show_radius_tower = null
+		else:
+			if show_radius_tower:
+				show_radius_tower.unselect()
+				show_radius_tower = null
+			if selected_tower != null:
+				selected_tower.unselect()
+				selected_tower = null
+			tower_offset = null
 	
 	if Input.is_action_just_released("LeftClick"):
 		if not did_move_mouse and selected_tower != null:
 			main.show_menu()
 		
-		selected_tower = null
 		did_move_mouse = false
 		last_mouse_pos = null
 		tower_offset = null
+		can_drag_tower = false
 	
 	if Input.is_action_just_pressed("PlayNextWave"):
 		wave_manager.play_next_wave()
